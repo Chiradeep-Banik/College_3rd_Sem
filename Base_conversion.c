f5f9c01eb21eeca579a1d224774be0386ae79df6
Chiradeep-Banik <chiradeep13@gmail.com>
2021-08-26 02:46:44 +0530

    trying_the_homework

--- Base_conversion.c
+++ Base_conversion.c
@@ -1,56 +1,73 @@
-#include<stdio.h>
+#include <stdio.h>
 
+//tomorrow -- make 2 function to convert all to decimal than chane it to required num
 
-char* convert(char num[], int base,int target){
-    int ar[1000];
-    if(base == target) return num;
-    printf("Part_1\n");
-    int i = 0;
-    while (num[i] != '\0'){
-        if(num[i] >= '0' && num[i] <= '9') ar[i] = num[i] - '0';
-        else if(num[i] >='A' && num[i] <= 'Z') ar[i] = num[i] - 'A' + 10;
-        else if(num[i] >='a' && num[i] <= 'z') ar[i] = num[i] - 'a' + 10;
-        i++;
+void rev(char *s,int n){
+    int i,j;
+    char temp;
+    for(i=0,j=n-1;i<j;i++,j--){
+        temp=s[i];
+        s[i]=s[j];
+        s[j]=temp;
     }
-    int length = i,number=0;
-    char result[length];
-    for(int j = 0 ; j <length;j++){
-        printf("%d ",ar[j]);
-        number = 10*number + ar[j];
-        if(ar[j] >= base) return "Invalid Input";
-    }
-    printf("-- Number: %d\n",number);
-
-    if(base > target){
-        printf("Part_2\n");
-        for(int k =0;k<length;k++){
-            printf("%d ",result[k]);            
-        }
-    }
-
-    // for(int j = 0; j < length; j++){
-    //     printf("%c",result[j]);
-    // }
-
-    return &result[0];
 }
 
-void main(){
-    int base,target;
+char *convert(char num[], int base, int target, char *result)
+{
+    int ar[1000];
+    if (base == target)
+        return num;
+    int i = 0;
+    while (num[i] != '\0')
+    {
+        if (num[i] >= '0' && num[i] <= '9')
+            ar[i] = num[i] - '0';
+        else if (num[i] >= 'A' && num[i] <= 'Z')
+            ar[i] = num[i] - 'A' + 10;
+        else if (num[i] >= 'a' && num[i] <= 'z')
+            ar[i] = num[i] - 'a' + 10;
+        i++;
+    }
+    int length = i, number = 0;
+    for (int j = 0; j < length; j++){
+        number = 10 * number + ar[j];
+        if (ar[j] >= base)
+            return "Invalid Input";
+    }
+    char temp[length*4];
+    if (base > target){
+        for(int i= 0 ; i < length*4 ; i++){
+            temp[i] = (number % target) + '0';
+            number = number / target;
+        }
+        rev(temp, length*4);
+        result = temp;
+    }
+
+    return result;
+}
+
+void main()
+{
+    int base, target;
     char num[100];
     printf("Enter the base(2,8,10,16): ");
-    scanf("%d",&base);
-    if(base != 2 && base != 8 && base != 10 && base != 16) printf("Error, enter valid bases\n");
-    else{
+    scanf("%d", &base);
+    if (base != 2 && base != 8 && base != 10 && base != 16)
+        printf("Error, enter valid bases\n");
+    else
+    {
         printf("Enter the target base(2,8,10,16): ");
-        scanf("%d",&target);
-        if(target != 2 && target != 8 && target != 10 && target != 16) printf("Error, enter valid bases\n");
-        else{
+        scanf("%d", &target);
+        if (target != 2 && target != 8 && target != 10 && target != 16)
+            printf("Error, enter valid bases\n");
+        else
+        {
             printf("Enter the number: ");
-            scanf("%s",num);
-            char* result;
-            result = convert(num,base,target,result);
-            printf("Base: %d Traget: %d Number: %s Result: %s\n",base,target,num);
+            scanf("%s", num);
+            char *result;
+            char *res = convert(num, base, target, result);
+            printf("Base: %d Traget: %d Number: %s Result: %s\n", base, target, num , res);
         }
     }
 }
