17976987d48ac19b7b12e5dbcfe4d21f79a76b2e
Chiradeep-Banik <chiradeep13@gmail.com>
2021-08-26 15:01:53 +0530

    cant_figure_out_the_addresses

--- Base_conversion.c
+++ Base_conversion.c
@@ -1,73 +1,66 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-//tomorrow -- make 2 function to convert all to decimal than chane it to required num
+void to_decimal(char* str){
+    printf("Addres : %p\n" , str);
 
-void rev(char *s,int n){
-    int i,j;
-    char temp;
-    for(i=0,j=n-1;i<j;i++,j--){
-        temp=s[i];
-        s[i]=s[j];
-        s[j]=temp;
-    }
 }
 
-char *convert(char num[], int base, int target, char *result)
-{
-    int ar[1000];
-    if (base == target)
-        return num;
-    int i = 0;
-    while (num[i] != '\0')
-    {
-        if (num[i] >= '0' && num[i] <= '9')
-            ar[i] = num[i] - '0';
-        else if (num[i] >= 'A' && num[i] <= 'Z')
-            ar[i] = num[i] - 'A' + 10;
-        else if (num[i] >= 'a' && num[i] <= 'z')
-            ar[i] = num[i] - 'a' + 10;
-        i++;
-    }
-    int length = i, number = 0;
-    for (int j = 0; j < length; j++){
-        number = 10 * number + ar[j];
-        if (ar[j] >= base)
-            return "Invalid Input";
-    }
-    char temp[length*4];
-    if (base > target){
-        for(int i= 0 ; i < length*4 ; i++){
-            temp[i] = (number % target) + '0';
-            number = number / target;
+void from_decimal(char *str,int target){
+    printf("Addres : %p\n" , str);
+    int number = atoi(str);
+    printf("NUm in 'from' : %d\n",number);
+    
+    int i =0;
+    while(1){
+        int rem = number%target;
+        number = (int)number/(int)target;
+        printf("REM : %d  ",rem);
+        printf("NUM : %d\n",number);
+        // number = number/target;
+        // printf("str in 'from': %s %d\n",str,i);
+        // i++;
+        if(number < target){
+            printf("REM : %d  ",rem);
+            printf("NUM : %d\n",number);
+            break;
         }
-        rev(temp, length*4);
-        result = temp;
     }
-
-    return result;
+    *(str+i) = number;
+    printf("str in 'from': %s\n",str);
 }
 
-void main()
-{
-    int base, target;
-    char num[100];
-    printf("Enter the base(2,8,10,16): ");
-    scanf("%d", &base);
-    if (base != 2 && base != 8 && base != 10 && base != 16)
-        printf("Error, enter valid bases\n");
-    else
-    {
-        printf("Enter the target base(2,8,10,16): ");
-        scanf("%d", &target);
-        if (target != 2 && target != 8 && target != 10 && target != 16)
-            printf("Error, enter valid bases\n");
-        else
-        {
-            printf("Enter the number: ");
-            scanf("%s", num);
-            char *result;
-            char *res = convert(num, base, target, result);
-            printf("Base: %d Traget: %d Number: %s Result: %s\n", base, target, num , res);
+char* convert(int base, int target,char* str){
+    printf("Addres : %p\n" , str);
+    from_decimal(str,target);
+    return str;
+}
+
+int main(){
+    int base,target;
+    char* num = malloc(sizeof(char)*100);
+    printf("Enter a base(2,8,10,16) : ");
+    scanf("%d",&base);
+    printf("Enter a target (2,8,10,16) : ");
+    scanf("%d",&target);
+    if(base != 2 && base != 8 && base != 10 && base != 16){
+        printf("Invalid base\n");
+    }else{
+        if(target != 2 && target != 8 && target != 10 && target != 16){
+            printf("Invalid target\n");
+        }else{
+            printf("Enter a number : ");
+            char ch;
+            int i =0;
+            while((ch = getchar()) != '\n'){
+                num[i] = ch;
+                i++;
+            }
         }
+        char* result = (char*)num;
+        printf("Addres : %p\n" , result);
+        printf("BASE : %d TARGET : %d NUMBER : %s RESULT : %s \n",base,target,num,convert(10,12,result));
     }
+
+    return 0;
 }
